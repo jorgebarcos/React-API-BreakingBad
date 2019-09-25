b97f788f11428bdcb975e7d89cd6527d9d75c023
Jorge Barcos <jorgebarcos88@gmail.com>
2019-09-25 18:22:12 -0300

    Consultando la API con useEffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+	const [ frase, obtenerFrase ] = useState({});
+
+	const consultarAPI = async () => {
+		const resultado = await axios('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+
+		//console.log(resultado.data[0]);
+
+		// agregar el resultado de la API al state; (similar a this,setState)
+		obtenerFrase(resultado.data[0]);
+	};
+
+	// consulta a una rest api
+
+	useEffect(() => {
+		consultarAPI();
+	}, []);
+	console.log(frase); // frase = this.state;
+
 	return <p>Hola</p>;
 }
 
