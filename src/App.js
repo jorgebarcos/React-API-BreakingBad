5c7a4c530208a8e4a44bd8c3194eece6baccb582
Jorge Barcos <jorgebarcos88@gmail.com>
2019-09-25 18:26:52 -0300

    Mostrando los Resultados de la Consulta

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+function Frase({ frase }) {
+	return (
+		<div className="frase">
+			<h1>{frase.quote}</h1>
+			<p>- {frase.author}</p>
+		</div>
+	);
+}
+
 function App() {
 	const [ frase, obtenerFrase ] = useState({});
 
@@ -18,9 +27,13 @@ function App() {
 	useEffect(() => {
 		consultarAPI();
 	}, []);
-	console.log(frase); // frase = this.state;
+	//console.log(frase); // frase = this.state;
 
-	return <p>Hola</p>;
+	return (
+		<div className="contenedor">
+			<Frase frase={frase} />
+		</div>
+	);
 }
 
 export default App;
